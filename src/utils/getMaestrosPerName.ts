d35febf279e3baa5957ca3956de923dc1d7589ea
Danilo de Araújo Leite Gomes <112290478+JayXani@users.noreply.github.com>
2025-07-24 23:28:07 -0300

    Refactor getTreeHTML function to generate a dynamic HTML structure with enhanced styling and functionality; remove getMaestrosPerName utility function.

--- src/utils/getMaestrosPerName.ts
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-export function getMaestrosPerName(basePath: string) {
-    const folders = fs.readdirSync(basePath);
-    return folders.map((folder) => folder.toLocaleUpperCase().includes("MAESTRO") ? folder : "");
-}
